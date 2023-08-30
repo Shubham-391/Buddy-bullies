f85f90dead74de11d96fc8946ac5f6e1d6939c19
Shubham-391 <subhamy1234@gmail.com>
2023-08-30 16:37:54 +0530

    accordian, email link, horizontal scroll bar and root.

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -5,3 +5,17 @@ icon.addEventListener("click", function () {
         view.classList.toggle("right-0");
     }
 })
+
+const accordianItems = document.querySelectorAll(".tab");
+
+accordianItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+        const isItemOpen = item.classList.contains("active"); //returns false
+        accordianItems.forEach(function (item) { item.classList.remove("active") });
+        if (!isItemOpen) //returns true
+        {
+            item.classList.toggle("active");
+        }
+    })
+});
+
